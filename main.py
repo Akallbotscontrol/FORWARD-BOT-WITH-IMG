f260a8f191b14c207602fc4fd4497fbac700c8c2
Ak all bots Control <akallbotscontrol@gmail.com>
2025-08-15 21:13:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,5 @@
-<p align="center">
-  <img src="https://i.postimg.cc/D0qQ884r/FORWARD-BOT-PIC-17.png" alt="Forward Bot Banner" width="550"/>
-</p>
-
-
-<b> AK AUTO Forward Bot</b>
-
-<b>Auto Restart All User Forwarding After Bot Restarted.</b>
-
-![Typing SVG](https://readme-typing-svg.herokuapp.com/?lines=Welcome+To+AK+AUTO+Forward+Bot+!)
-
-## How To Deploy [Video Tutorial](https://youtu.be/A-iIh_5WAlk)
-
-## Features
-
-- [x] Public Forward (Bot)
-- [x] Private Forward (User Bot)
-- [x] Custom Caption 
-- [x] Custom Button
-- [x] Skip Duplicate Messages
-- [x] Skip Messages Based On Extensions & Keywords & Size
-- [x] Filter Type Of Messages
-- [x] Auto Restart Pending Task After Bot Restart 
-
-
-<b>To Know About All Features, Join My <a href='https://t.me/AK_Botz_UPDATE'>Update Channel</a>.</b>
-
-## Commands
-
-```
-start - check I'm alive 
-forward - forward messages
-unequify - delete duplicate media messages in chats
-settings - configure your settings
-stop - stop your ongoing tasks
-reset - reset your settings
-restart - restart server (owner only)
-resetall - reset all users settings (owner only)
-broadcast - broadcast a message to all your users (owner only)
-```
-
-## Variables
-
-* `API_ID` API Id from my.telegram.org
-* `API_HASH` API Hash from my.telegram.org
-* `BOT_TOKEN` Bot token from @BotFather
-* `BOT_OWNER` Telegram Account Id of Owner.
-* `DATABASE_URI` Database uri from [MongoDB](https://mongodb.com) Watch [Video Tutorial](https://youtu.be/DAHRmFdw99o)
-
-## Credits
-
-* <b>[MR ABHAY](https://t.me/mr_abhay_k)</b>
-
+# Don't Remove Credit Tg - @MR_ABHAY_K
+# Ask Doubt on telegram @AK_BOTZ_SUPPORT
 
 import asyncio, logging
 from config import Config
@@ -59,9 +8,8 @@ from typing import Union, Optional, AsyncGenerator
 from logging.handlers import RotatingFileHandler
 from plugins.regix import restart_forwards
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @MR_ABHAY_K
+# Ask Doubt on telegram @AK_BOTZ_SUPPORT
 
 if __name__ == "__main__":
     VJBot = VJ(
@@ -120,7 +68,6 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+    
+# # Don't Remove Credit Tg - @MR_ABHAY_K
+# Ask Doubt on telegram @AK_BOTZ_SUPPORT
